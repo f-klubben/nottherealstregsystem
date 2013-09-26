7ffa903132cf84139b586f14bd9f5382c48b8545
Henrik Ossipoff Hansen <hoha@entertainment-trading.com>
2013-09-26 19:18:53 +0200

    Updated settings
    - Settings now largely uses environment variables
    - Setup Cling for static serves

--- stregsystem/settings.py
+++ stregsystem/settings.py
@@ -1,31 +1,30 @@
-"""
-Django settings for stregsystem project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/dev/ref/settings/
-"""
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f@!&-fi*cnctjrgsf**&@ct$fa2ym1&9@8%-8#2-&h2l9270n#'
+try:
+    SECRET_KEY = os.environ['DJANGO_SECRET']
+except:
+    SECRET_KEY = 'f@!&-fi*cnctjrgsf**&@ct$fa2ym1&9@8%-8#2-&h2l9270n#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+try:
+    DEBUG = bool(int(os.environ['DJANGO_DEBUG']))
+except:
+    DEBUG = True
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+ALLOWED_HOSTS = ['stregsystem.fklub.dk']
 
 # Application definition
 
@@ -55,17 +54,25 @@ WSGI_APPLICATION = 'stregsystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+import dj_database_url
+if len(dj_database_url.config()) > 0:
+    DATABASES = {
+        'default': dj_database_url.config()
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+_ = lambda s: s
+
+LANGUAGE_CODE = 'da-DK'
 
 TIME_ZONE = 'UTC'
 
@@ -75,8 +82,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
