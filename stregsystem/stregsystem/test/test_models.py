22d3db1c2bea49366039a9be8a0cb1183219d326
Mark Faldborg <MarkFaldborg@gmail.com>
2015-02-24 13:50:37 +0100

    Added a bunch of stuff to Models
    I added a Member object not dissimmilar to the one in 3.4.5 (or w/e)
    however it has fewer fields:
     * want_spam is gone, it should apparently not be used.
     * year was replaced by join_date
     * undo_count has stopped being useful
     * balance should probably be a cached calculation rather than a field
     * gender was never used for anything useful
     * notes is replaced by note to follow django naming conventions
     * firstname/lastname are supplanted by name (simpler)
    
    New fields include pin and phone number, they are used for unique
    identification of members in situations like payment or sale.
    
    The two new fields also allow some more complex behaviour, e.g. removing
    formatting in phone number input or ensuring that pin is not saved as
    plain text but as a hash.
    
    Two validation functions were added, they do simple validation of pin
    and phone number.
    
    Phone number related computation could be moved out of Member since they
    could be useful to other systems.

---
+++ stregsystem/stregsystem/test/test_models.py
@@ -0,0 +1,80 @@
+from django.test import TestCase
+from stregsystem import models
+from django.core.exceptions import ValidationError
+
+class MemberPhoneNumberTestCase(TestCase):
+    def  test_phone_number_set_success_space(self):
+        m = models.Member(phone_number='12 34')
+        self.assertEqual(m.phone_number, '1234')
+
+    def  test_phone_number_set_success_dash(self):
+        m = models.Member(phone_number='12-34')
+        self.assertEqual(m.phone_number, '1234')
+
+    def  test_phone_number_set_success_underscore(self):
+        m = models.Member(phone_number='12_34')
+        self.assertEqual(m.phone_number, '1234')
+
+    def test_phone_number_format_even(self):
+        m = models.Member(phone_number='1234')
+        self.assertEqual(m.phone_number_format(), '12 34')
+
+    def test_phone_number_format_odd(self):
+        m = models.Member(phone_number='123')
+        self.assertEqual(m.phone_number_format(), '1 23')
+
+    def test_phone_number_validator_fail_nothing(self):
+        with self.assertRaises(ValidationError):
+            models.validate_phone_number('')
+
+    def test_phone_number_validator_fail_long(self):
+        with self.assertRaises(ValidationError):
+            models.validate_phone_number('12345678901234567')
+
+    def test_phone_number_validator_fail_letters(self):
+        # Recall that this is not a likely thing to happen.
+        with self.assertRaises(ValidationError):
+            models.validate_phone_number('1234567a')
+
+    def test_phone_number_validator_success_short(self):
+        self.assertEqual(models.validate_phone_number('1'), None)
+
+    def test_phone_number_validator_success_middle(self):
+        self.assertEqual(models.validate_phone_number('12345678'), None)
+
+    def test_phone_number_validator_success_long(self):
+        self.assertEqual(models.validate_phone_number('1234567890123456'), None)
+
+
+class MemberPinTestCase(TestCase):
+    def setUp(self):
+        self.member = models.Member(pin='1234')
+
+    def test_check_pin_fail(self):
+        self.assertFalse(self.member.check_pin('4321'))
+
+    def test_check_pin_success(self):
+        self.assertTrue(self.member.check_pin('1234'))
+
+    def test_change_pin_fail(self):
+        self.assertFalse(self.member.change_pin('2234','4321'))
+        self.assertFalse(self.member.check_pin('4321'))
+
+    def test_change_pin_success(self):
+        self.assertTrue(self.member.change_pin('1234','4321'))
+        self.assertTrue(self.member.check_pin('4321'))
+
+    def test_pin_validator_fail_short(self):
+        with self.assertRaises(ValidationError):
+            models.validate_pin('123')
+
+    def test_pin_validator_fail_long(self):
+        with self.assertRaises(ValidationError):
+            models.validate_pin('12345')
+
+    def test_pin_validator_fail_letters(self):
+        with self.assertRaises(ValidationError):
+            models.validate_pin('abcd')
+
+    def test_pin_validator_success(self):
+        self.assertEqual(models.validate_pin('1234'), None)
