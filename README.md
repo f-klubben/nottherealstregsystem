637223fc4e7c65996b9ed700a57ed1cfe8f62daa
Mark Faldborg <MarkFaldborg@gmail.com>
2015-04-01 12:47:40 +0200

    Removed python dependancies.

--- README.md
+++
@@ -1,5 +0,0 @@
-Stregsystem
-===========
-[![Build Status](https://travis-ci.org/HenrikOssipoff/stregsystem.svg?branch=master)](https://travis-ci.org/HenrikOssipoff/stregsystem)
-
-Stregsystrem v.4
--- stregsystem/catalog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Listing, PointOfSale, Product, Tag
-
-admin.site.register(Product)
-admin.site.register(PointOfSale)
-admin.site.register(Listing)
-admin.site.register(Tag)
